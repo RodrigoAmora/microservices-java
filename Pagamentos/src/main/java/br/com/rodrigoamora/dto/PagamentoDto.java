875cc84802af1b402703880c784040fce992cf27
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-17 00:06:40 -0300

    Criando a classe PagamentoDto

--- Pagamentos/src/main/java/br/com/rodrigoamora/dto/PagamentoDto.java
+++ Pagamentos/src/main/java/br/com/rodrigoamora/dto/PagamentoDto.java
@@ -1,5 +1,23 @@
 package br.com.rodrigoamora.dto;
 
+import java.math.BigDecimal;
+
+import br.com.rodrigoamora.model.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PagamentoDto {
 
+	private Long id;
+	private BigDecimal valor;
+	private String nome;
+	private String numero;
+	private String expiracao;
+	private String codigo;
+	private Status status;
+	private Long formaDePagamentoId;
+	private Long pedidoId;
+	
 }
