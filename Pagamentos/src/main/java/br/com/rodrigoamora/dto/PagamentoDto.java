8f8376aef6b572dd1550ef0f81557ddd02ff14cb
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-17 01:40:59 -0300

    Criando getters e setters

--- Pagamentos/src/main/java/br/com/rodrigoamora/dto/PagamentoDto.java
+++ Pagamentos/src/main/java/br/com/rodrigoamora/dto/PagamentoDto.java
@@ -3,11 +3,7 @@ package br.com.rodrigoamora.dto;
 import java.math.BigDecimal;
 
 import br.com.rodrigoamora.model.Status;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class PagamentoDto {
 
 	private Long id;
@@ -20,4 +16,77 @@ public class PagamentoDto {
 	private Long formaDePagamentoId;
 	private Long pedidoId;
 	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public BigDecimal getValor() {
+		return valor;
+	}
+	
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getExpiracao() {
+		return expiracao;
+	}
+	
+	public void setExpiracao(String expiracao) {
+		this.expiracao = expiracao;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public Status getStatus() {
+		return status;
+	}
+	
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	
+	public Long getFormaDePagamentoId() {
+		return formaDePagamentoId;
+	}
+	
+	public void setFormaDePagamentoId(Long formaDePagamentoId) {
+		this.formaDePagamentoId = formaDePagamentoId;
+	}
+	
+	public Long getPedidoId() {
+		return pedidoId;
+	}
+	
+	public void setPedidoId(Long pedidoId) {
+		this.pedidoId = pedidoId;
+	}
+
+
 }
