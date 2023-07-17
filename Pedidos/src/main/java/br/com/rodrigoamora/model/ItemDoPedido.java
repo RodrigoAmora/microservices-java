7bf43e282ce4b08fa1874294f16ea5a00b954bc5
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-17 04:33:51 -0300

    Classes de modelo

---
+++ Pedidos/src/main/java/br/com/rodrigoamora/model/ItemDoPedido.java
@@ -0,0 +1,60 @@
+package br.com.rodrigoamora.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name = "item_do_pedido")
+public class ItemDoPedido {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Positive
+    private Integer quantidade;
+
+    private String descricao;
+
+    @ManyToOne(optional=false)
+    private Pedido pedido;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+}
---
+++ Pedidos/src/main/java/br/com/rodrigoamora/model/Status.java
@@ -0,0 +1,12 @@
+package br.com.rodrigoamora.model;
+
+public enum Status {
+    REALIZADO,
+    CANCELADO,
+    PAGO,
+    NAO_AUTORIZADO,
+    CONFIRMADO,
+    PRONTO,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE;
+}
