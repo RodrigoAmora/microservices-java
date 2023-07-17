a833f4027c1bc369b5d513cd44da83a0b36e953f
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-17 01:41:15 -0300

    Classe Configuracao

---
+++ Pagamentos/src/main/java/br/com/rodrigoamora/config/Configuracao.java
@@ -0,0 +1,15 @@
+package br.com.rodrigoamora.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configuracao {
+	
+	@Bean
+	public ModelMapper obterModelMapper() {
+		return new ModelMapper();
+	}
+
+}
