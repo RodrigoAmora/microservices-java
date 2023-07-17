36cf47a0011e419c952a215f05d768d27e98b3bb
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-17 04:34:54 -0300

    Classes de Repository

---
+++ Pedidos/src/main/java/br/com/rodrigoamora/repository/PedidoRepository.java
@@ -0,0 +1,22 @@
+package br.com.rodrigoamora.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.rodrigoamora.model.Pedido;
+import br.com.rodrigoamora.model.Status;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+	
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Pedido p set p.status = :status where p = :pedido")
+    void atualizaStatus(Status status, Pedido pedido);
+
+    @Query(value = "SELECT p from Pedido p LEFT JOIN FETCH p.itens where p.id = :id")
+    Pedido porIdComItens(Long id);
+
+
+}
