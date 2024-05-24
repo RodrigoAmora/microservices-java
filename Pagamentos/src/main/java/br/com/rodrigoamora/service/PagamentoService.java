0527905925a4f5b0a7fdf6f6a68b3da5d7eaa0e2
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-24 00:29:46 -0300

    Renomeando o atributo pagamentoRepository

--- Pagamentos/src/main/java/br/com/rodrigoamora/service/PagamentoService.java
+++ Pagamentos/src/main/java/br/com/rodrigoamora/service/PagamentoService.java
@@ -19,7 +19,7 @@ import jakarta.persistence.EntityNotFoundException;
 public class PagamentoService {
 	
 	@Autowired
-    private PagamentoRepository repository;
+    private PagamentoRepository pagamentoRepository;
 	
 	@Autowired
 	private ModelMapper modelMapper;
@@ -28,13 +28,13 @@ public class PagamentoService {
 	private PedidoClient pedido;
 	
 	public Page<PagamentoDto> obterTodos(Pageable paginacao) {
-		return repository
-                .findAll(paginacao)
-                .map(p -> modelMapper.map(p, PagamentoDto.class));
+		return this.pagamentoRepository
+                	.findAll(paginacao)
+                	.map(p -> modelMapper.map(p, PagamentoDto.class));
     }
 	 
 	 public PagamentoDto obterPorId(Long id) {
-		 Pagamento pagamento = repository.findById(id)
+		 Pagamento pagamento = pagamentoRepository.findById(id)
 				 						.orElseThrow(() -> new EntityNotFoundException());
 
 		 return modelMapper.map(pagamento, PagamentoDto.class);
@@ -43,7 +43,8 @@ public class PagamentoService {
 	 public PagamentoDto criarPagamento(PagamentoDto dto) {
 		 Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
 		 pagamento.setStatus(Status.CRIADO);
-		 repository.save(pagamento);
+		 
+		 this.pagamentoRepository.save(pagamento);
 
         return modelMapper.map(pagamento, PagamentoDto.class);
 	 }
@@ -51,34 +52,35 @@ public class PagamentoService {
 	 public PagamentoDto atualizarPagamento(Long id, PagamentoDto dto) {
 		 Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
 		 pagamento.setId(id);
-		 pagamento = repository.save(pagamento);
+		 pagamento = this.pagamentoRepository.save(pagamento);
 		 return modelMapper.map(pagamento, PagamentoDto.class);
 	 }
 	 
 	 public void excluirPagamento(Long id) {
-		 repository.deleteById(id);
+		 this.pagamentoRepository.deleteById(id);
 	 }
 	 
 	 public void confirmarPagamento(Long id) {
-		 Optional<Pagamento> pagamento = repository.findById(id);
+		 Optional<Pagamento> pagamento = this.pagamentoRepository.findById(id);
 		 
 		 if (!pagamento.isPresent()) {
 			 throw new EntityNotFoundException();
 	      }
 
 		 pagamento.get().setStatus(Status.CONFIRMADO);
-		 repository.save(pagamento.get());
+		 this.pagamentoRepository.save(pagamento.get());
 		 pedido.atualizaPagamento(pagamento.get().getPedidoId());
 	 }
 	 
 	 public void alteraStatus(Long id) {
-		 Optional<Pagamento> pagamento = repository.findById(id);
+		 Optional<Pagamento> pagamento = this.pagamentoRepository.findById(id);
 
 		 if (!pagamento.isPresent()) {
 			 throw new EntityNotFoundException();
    		 }
 
 		 pagamento.get().setStatus(Status.CONFIRMADO_SEM_INTEGRACAO);
-		 repository.save(pagamento.get());
+		 this.pagamentoRepository.save(pagamento.get());
     }
+	 
 }
