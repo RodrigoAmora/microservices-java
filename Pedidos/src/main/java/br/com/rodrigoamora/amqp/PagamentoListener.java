e8b9b61f2ff6c4c907e2c9ad38d3fd9bcb313d98
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-06-22 20:23:45 -0300

    Implementando a classe PagamentoListener

---
+++ Pedidos/src/main/java/br/com/rodrigoamora/amqp/PagamentoListener.java
@@ -0,0 +1,15 @@
+package br.com.rodrigoamora.amqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagamentoListener {
+
+	@RabbitListener(queues = "pagamento.concluido")
+	public void recebeMensagem(Message mensagem) {
+		System.out.println("Recebi a mensagem " + mensagem.toString());
+    }
+	
+}
