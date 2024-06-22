88f40e348645210dbdce67ca255d9c1775100978
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-06-22 19:11:15 -0300

    Implementando a classe PagamentoAMQPConfiguration

--- Pagamentos/src/main/java/br/com/rodrigoamora/amqp/PagamentoAMQPConfiguration.java
+++ Pagamentos/src/main/java/br/com/rodrigoamora/amqp/PagamentoAMQPConfiguration.java
@@ -2,17 +2,22 @@ package br.com.rodrigoamora.amqp;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PagamentoAMQPConfiguration {
 
 	@Bean
 	Queue criaFila() {
+		/*
+		 * Criando uma fila não durável
+		 * Ambas as linhas fazem a mesma coisa
+		 */
 		//return new Queue ("pagamento.concluido", false);
 		return QueueBuilder.nonDurable("pagamento.concluido").build();
 	}
@@ -20,6 +25,11 @@ public class PagamentoAMQPConfiguration {
 	@Bean
     RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
 		return new RabbitAdmin(conn);
+	}
+	
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin){
+		return event -> rabbitAdmin.initialize();
 	}
 	
 }
