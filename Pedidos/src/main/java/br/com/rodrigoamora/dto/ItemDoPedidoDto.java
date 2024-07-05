26eb4e95b7e4e82cc3b5ac0727b2c428e1fa7c4f
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-07-05 02:02:00 -0300

    Adicionando Getters e Setters

--- Pedidos/src/main/java/br/com/rodrigoamora/dto/ItemDoPedidoDto.java
+++ Pedidos/src/main/java/br/com/rodrigoamora/dto/ItemDoPedidoDto.java
@@ -1,9 +1,36 @@
 package br.com.rodrigoamora.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ItemDoPedidoDto {
 
+	@JsonIgnore
     private Long id;
     private Integer quantidade;
     private String descricao;
+    
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+    
 }
