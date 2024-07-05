cf2701a188e842af264795fd5587d717434e798f
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-07-05 14:38:02 -0300

    Adicionando a anotação EnableFeignClients

--- Pagamentos/src/main/java/br/com/rodrigoamora/PagamentosApplication.java
+++ Pagamentos/src/main/java/br/com/rodrigoamora/PagamentosApplication.java
@@ -3,9 +3,11 @@ package br.com.rodrigoamora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
