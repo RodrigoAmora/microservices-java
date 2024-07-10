ed1696c24c06db580cceccaffb56a32c6a9b352d
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-07-10 00:31:34 -0300

    Aprimorando o mensagem recebida do RabbitMQ

--- Pedidos/src/main/java/br/com/rodrigoamora/amqp/PagamentoListener.java
+++ Pedidos/src/main/java/br/com/rodrigoamora/amqp/PagamentoListener.java
@@ -1,15 +1,26 @@
 package br.com.rodrigoamora.amqp;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import br.com.rodrigoamora.dto.PagamentoDto;
 
 @Component
 public class PagamentoListener {
 
 	@RabbitListener(queues = "pagamento.concluido")
-	public void recebeMensagem(Message mensagem) {
-		System.out.println("Recebi a mensagem " + mensagem.toString());
+	public void recebeMensagem(PagamentoDto pagamento) {
+		String mensagem = """
+				Dados do pagamento: %s
+				Numero pedido: %s
+				Valor R$: %s
+				Status: $s
+				""".formatted(pagamento.getId(),
+							  pagamento.getNumero(),
+							  pagamento.getValor(),
+							  pagamento.getStatus());
+		
+		System.out.println("Recebi a mensagem " + mensagem);
     }
 	
 }
