5cd09077ac2ec2377658de3650ddfb3f0a7f6555
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-07-26 00:30:23 -0300

    Serviço de Avaliação

---
+++ Avaliacao/src/test/java/br/com/rodrigoamora/AvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
