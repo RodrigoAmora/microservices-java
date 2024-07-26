14abbdefdb53581136a91872779a2a133a83e059
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-07-26 00:42:49 -0300

    Criando as classe de modelo

---
+++ Avaliacao/src/main/java/br/com/rodrigoamora/dto/PagamentoDto.java
@@ -0,0 +1,97 @@
+package br.com.rodrigoamora.dto;
+
+import java.math.BigDecimal;
+
+public class PagamentoDto {
+
+    private Long id;
+    
+    private BigDecimal valor;
+    
+    private String nome;
+    
+    private String numero;
+    
+    private String expiracao;
+    
+    private String codigo;
+    
+    private StatusPagamento status;
+    
+    private Long formaDePagamentoId;
+    
+    private Long pedidoId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getExpiracao() {
+		return expiracao;
+	}
+
+	public void setExpiracao(String expiracao) {
+		this.expiracao = expiracao;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public StatusPagamento getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusPagamento status) {
+		this.status = status;
+	}
+
+	public Long getFormaDePagamentoId() {
+		return formaDePagamentoId;
+	}
+
+	public void setFormaDePagamentoId(Long formaDePagamentoId) {
+		this.formaDePagamentoId = formaDePagamentoId;
+	}
+
+	public Long getPedidoId() {
+		return pedidoId;
+	}
+
+	public void setPedidoId(Long pedidoId) {
+		this.pedidoId = pedidoId;
+	}
+    
+}
---
+++ Avaliacao/src/main/java/br/com/rodrigoamora/dto/StatusPagamento.java
@@ -0,0 +1,8 @@
+package br.com.rodrigoamora.dto;
+
+public enum StatusPagamento {
+    CRIADO,
+    CONFIRMADO,
+    CONFIRMADO_SEM_INTEGRACAO,
+    CANCELADO
+}
