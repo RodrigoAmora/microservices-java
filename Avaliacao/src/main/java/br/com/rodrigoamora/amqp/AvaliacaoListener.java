802ec755dce78fbd5731cc827ee299364961d2ca
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-07-26 00:43:48 -0300

    Implementando as classe de comunicação com o RabbitMQ

---
+++ Avaliacao/src/main/java/br/com/rodrigoamora/amqp/AvaliacaoListener.java
@@ -0,0 +1,27 @@
+package br.com.rodrigoamora.amqp;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import br.com.rodrigoamora.dto.PagamentoDto;
+
+@Component
+public class AvaliacaoListener {
+    @RabbitListener(queues = "pagamentos.detalhes-avaliacao")
+    public void recebeMensagem(@Payload PagamentoDto pagamento) {
+        String mensagem = """
+                Necessário criar registro de avaliação para o pedido: %s 
+                Id do pagamento: %s
+                Nome do cliente: %s
+                Valor R$: %s
+                Status: %s 
+                """.formatted(pagamento.getPedidoId(),
+                pagamento.getId(),
+                pagamento.getNome(),
+                pagamento.getValor(),
+                pagamento.getStatus());
+
+        System.out.println(mensagem);
+    }
+}
